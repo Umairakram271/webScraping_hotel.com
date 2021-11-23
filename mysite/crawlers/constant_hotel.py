a0a862c075ee51d57be42db3860ff8cc2024a1d1
“[umir]” <[umair@wanologicalsolutions.com]>
2021-11-23 19:00:11 +0500

    hotel.com webScraping final code csv code

--- mysite/crawlers/constant_hotel.py
+++ mysite/crawlers/constant_hotel.py
@@ -1,26 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import csv
+
+
 def crawler():
-    driver = webdriver.Chrome(executable_path='/home/akmal/Desktop/crawler/mysite/crawlers/chromedriver.exe')
-    driver.get('https://www.hotels.com/search.do?destination-id=1693203&q-'
-                         'check-in=2021-11-24&q-check-out=2021-11-25&q-rooms=1&q-room-0-'
-                         'adults=2&q-room-0-children=0&sort-order=BEST_SELLER')
+    driver = webdriver.Chrome(executable_path='/home/akmal/Desktop/crawler/webScraping_hotel.com/mysite/'
+                                              'crawlers/chromedriver.exe')
+    driver.get('https://www.hotels.com/search.do?destination-id=1642097&q-check-in=2021-11-25&q-check-out=2021-11-26&q-rooms=1&q-room-0-adults=2&q-room-0-children=0&sort-order=BEST_SELLER')
     print(driver.title)
     import pdb; pdb.set_trace()
-    blockData = driver.find_elements_by_css_selector('div.-RcIiD')
-    print(blockData)
+    block_data = driver.find_elements_by_css_selector('div.-RcIiD')
+    print(block_data)
 
     hotel_list = []
-    for z in blockData:
+    for z in block_data:
         row_data = []
         try:
-            hotelTitile = z.find_element_by_css_selector('h2._3zH0kn').text
-            if hotelTitile == '':
-                hotelTitile = "N/A"
-            print(hotelTitile)
+            hotel_title = z.find_element_by_css_selector('h2._3zH0kn').text
+            if hotel_title == '':
+                hotel_title = "N/A"
+            print(hotel_title)
         except:
-            hotelTitile = "N/A"
+            hotel_title = "N/A"
         try:
             address = z.find_element_by_css_selector('p._2oHhXM').text
             if address == '':
@@ -29,12 +30,12 @@ def crawler():
         except:
             address = "N/A"
         try:
-            placeTitle = z.find_element_by_css_selector('p._2LH_yj').text
-            if placeTitle == '':
-                placeTitle = "N/A"
-            print(placeTitle)
+            place_title = z.find_element_by_css_selector('p._2LH_yj').text
+            if place_title == '':
+                place_title = "N/A"
+            print(place_title)
         except:
-            placeTitle = "N/A"
+            place_title = "N/A"
         try:
             rang_title = z.find_element_by_css_selector('ul._2sHYiJ._3JLCGC').text
             if rang_title == '':
@@ -58,15 +59,25 @@ def crawler():
             status = "N/A"
         try:
             parking = z.find_element_by_css_selector('ul._1CyeN6').text
-            parking_detail = parking.replace("'\ue940\n", " ")
-            parking_detail = parking.replace("\ue950\n", " ")
-            parking_detail = parking.replace("\ue935\n", " ")
             parking_detail = parking.replace("\ue940\n", " ")
-            if parking == "":
-                parking = "N/A"
-            print(parking)
+            parking_detail = parking_detail.replace("\ue950\n", " ")
+            parking_detail = parking_detail.replace("\ue935\n", " ")
+            parking_detail = parking_detail.replace("\ue95b\n", " ")
+            parking_detail = parking_detail.replace("\ue9b2\n", " ")
+            parking_detail = parking_detail.replace("\ue90a\n", " ")
+            parking_detail = parking_detail.replace("\ue949\n", " ")
+            parking_detail = parking_detail.replace("\ue945\n", " ")
+            parking_detail = parking_detail.replace("\ue92f\n", " ")
+            parking_detail = parking_detail.replace("\ue933\n", " ")
+            parking_detail = parking_detail.replace("\ue93e\n", " ")
+            parking_detail = parking_detail.replace("\ue93f\n", " ")
+            print(parking_detail)
+
+            if parking_detail == "":
+                parking_detail = "N/A"
+            print(parking_detail)
         except:
-            parking = "N/A"
+            parking_detail = "N/A"
         try:
             price = z.find_element_by_css_selector("span._2R4dw5").text
             if price == "":
@@ -74,34 +85,32 @@ def crawler():
             print(price)
         except:
             price = "N/A"
-        if len(hotelTitile) > 0:
-            row_data.append(hotelTitile)
+        if len(hotel_title) > 0:
+            row_data.append(hotel_title)
         if len(address) > 0:
             row_data.append(address)
-        if len(placeTitle) > 0:
-            row_data.append(placeTitle)
+        if len(place_title) > 0:
+            row_data.append(place_title)
         if len(rang_title) > 0:
             row_data.append(rang_title)
         if len(reating) > 0:
             row_data.append(reating)
         if len(status) > 0:
             row_data.append(status)
-        if len(parking) > 0:
-            row_data.append(parking)
+        if len(parking_detail) > 0:
+            row_data.append(parking_detail)
         if len(price) > 0:
             row_data.append(price)
         print(row_data)
         hotel_list.append(row_data)
         print(hotel_list)
-    import pdb;pdb.set_trace()
-    header = ['hotelTitile', 'address', 'placeTitle','rang_title','reating','status','parking','price']
-    with open('murree.csv', 'w', encoding='UTF8', newline='') as f:
+    header = ['hotel_title', 'address', 'placeTitle','rang_title','reating','status','parking','price']
+    with open('Abbotabad.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
 
         # write the data
         for j in hotel_list:
-            import pdb;
-            pdb.set_trace()
             writer.writerow(j)
+
     driver.quit()
